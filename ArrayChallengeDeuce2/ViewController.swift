6ee291708ec2e54407e147a3787e4a9bc466fd38
SusanZheng15 <zheng.susan12@gmail.com>
2016-07-08 10:36:26 -0400

    Done

--- ArrayChallengeDeuce2/ViewController.swift
+++ ArrayChallengeDeuce2/ViewController.swift
@@ -8,19 +8,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController
+{
     // This represents our deliLine.
     var deliLine: [String] = []
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
     }
 
-   
+    func addNameToDeliLine(name: String)-> String
+    {
+        if name == "Billy Crystal"
+        {
+            deliLine.insert(name, atIndex: 0)
+            return "Welcome Billy! You can sit wherever you like."
+        }
+        else if name == "Meg Ryan"
+        {
+            deliLine.insert(name, atIndex: 0)
+            return "Welcome Meg! You can sit wherever you like."
+        }
+        else
+        {
+            deliLine.append(name)
+            
+            if deliLine.count == 1
+            {
+                return "Welcome \(name), you're first in line!"
+            }
+            else
+            {
+                return "Welcome \(name), you're number \(deliLine.count) in line."
+            }
+            
+        }
+        
+    }
     
     
-    // Create your methods here
+    func nowServing() -> String
+    {
+        if deliLine.isEmpty
+        {
+            return "There is no-one to be served."
+        }
+        else
+        {
+            return "Now serving \(deliLine.last)!"
+        }
+        
+    }
+    
+    
+    func deliLineDescription() -> String
+    {
+        if deliLine.isEmpty
+        {
+            return "The line is currently empty."
+        }
+        else
+        {
+            var newLine: String = ""
+            
+            for (index, name) in deliLine.enumerate()
+            {
+                newLine = "\(newLine)\(index). \(name)"
+            }
+            return "The line is: \(newLine)"
+        }
+        
+    }
 
 
 }
